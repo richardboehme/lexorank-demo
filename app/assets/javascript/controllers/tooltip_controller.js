e11acb8c863fbafcbbdc9c6bd7a51f6779afe558
Richard Böhme <richard.boehme1999@gmail.com>
2021-03-26 17:00:51 +0100

    remove tooltip workaround

--- app/assets/javascript/controllers/tooltip_controller.js
+++ app/assets/javascript/controllers/tooltip_controller.js
@@ -3,7 +3,6 @@ import Tooltip from "bootstrap/js/dist/tooltip";
 
 export default class extends Controller {
   connect() {
-    // TODO: see https://github.com/twbs/bootstrap/issues/32372
-    new Tooltip(this.element, { animation: false });
+    new Tooltip(this.element);
   }
 }
