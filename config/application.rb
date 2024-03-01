6b7fa50121ca12d8a8cdb964eab102447b7647d9
Richard Böhme <richard.boehme1999@gmail.com>
2024-03-01 23:23:08 +0100

    Update rails to 7.1

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require 'action_controller/railtie'
 # require "action_text/engine"
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,13 +23,13 @@ Bundler.require(*Rails.groups)
 module LexorankSampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.session_store :active_record_store, key: 'lexorank_demo'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
   end
 end
